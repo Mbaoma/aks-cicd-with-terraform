bb0f7b120e6f4972b2ddb4a2c77084d60dd30c1c
Mbaoma <mmaryraphaella@gmail.com>
2024-01-28 14:53:00 +0100

    public aks

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -57,48 +57,6 @@ resource "azurerm_kubernetes_cluster" "lights_on_heights_aks" {
   }
 }
 
-# setup cluster with RBAC and monitoring enabled
-# resource "azurerm_kubernetes_cluster" "lights_on_heights_aks" {
-#   name                = var.cluster_name
-#   location            = azurerm_resource_group.lights_on_heights_aks_rg.location
-#   resource_group_name = azurerm_resource_group.lights_on_heights_aks_rg.name
-#   dns_prefix          = var.cluster_dns_prefix
-
-#   default_node_pool {
-#     name                = "aksnp"
-#     node_count          = 1 # Initial node count
-#     vm_size             = "Standard_DS2_v2"
-#     vnet_subnet_id      = module.networking.subnet_id
-#     enable_auto_scaling = true # Enable cluster autoscaler
-#     min_count           = 1    # Minimum number of nodes
-#     max_count           = 2    # Maximum number of nodes
-#   }
-
-#   role_based_access_control_enabled = true
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-
-  # network_profile {
-  #   network_plugin = "azure"
-  #   network_policy = "calico"
-  #   service_cidr       = "10.1.0.0/16"
-  #   dns_service_ip     = "10.1.0.10"
-  #   docker_bridge_cidr = "172.17.0.1/16"
-  # }
-
-#   network_profile {
-#     network_plugin     = "azure"
-#     network_policy     = "calico"
-#     load_balancer_sku  = "standard"  # Standard load balancer for public IP
-#   }
-
-#   oms_agent {
-#     log_analytics_workspace_id = azurerm_log_analytics_workspace.lights_on_heights_log_analytics.id
-#   }
-# }
-
 # Setup Cluster Monitoring
 resource "azurerm_log_analytics_workspace" "lights_on_heights_log_analytics" {
   name                = "lights-on-heights-aks-logs"
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -5,22 +5,13 @@ module "resource_group" {
   location = var.location_rg
 }
 
-# Virtual Network Resource
-resource "azurerm_virtual_network" "lights_on_heights_vnet" {
-  name                = var.vnet_name
-  location            = module.resource_group.location
-  resource_group_name = module.resource_group.name
-  address_space       = var.vnet_address_space
-  dns_servers         = var.dns_servers
-}
-
 # Network Security Group Resource
 resource "azurerm_network_security_group" "lights_on_heights_sg" {
   name                = var.network_sg_name
   location            = module.resource_group.location
   resource_group_name = module.resource_group.name
 
-  # Existing inbound rule
+  # inbound rule
   security_rule {
     name                       = "allow-aks-required-inbound"
     priority                   = 100
@@ -28,12 +19,12 @@ resource "azurerm_network_security_group" "lights_on_heights_sg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_ranges    = ["80", "443", "22", "53", "9000"]
+    destination_port_ranges    = ["80", "443", "22", "53"]
     source_address_prefix      = "VirtualNetwork"
     destination_address_prefix = "VirtualNetwork"
   }
 
-  # Example outbound rule to allow all outbound traffic
+  # outbound rule to allow all outbound traffic
   security_rule {
     name                       = "allow-all-outbound"
     priority                   = 101
@@ -47,12 +38,33 @@ resource "azurerm_network_security_group" "lights_on_heights_sg" {
   }
 }
 
+# Virtual Network Resource
+resource "azurerm_virtual_network" "lights_on_heights_vnet" {
+  name                = var.vnet_name
+  location            = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  address_space       = var.vnet_address_space
+  dns_servers         = var.dns_servers
+}
+
+
+
 # Subnet Resource
 resource "azurerm_subnet" "lights_on_heights_subnet" {
   name                 = var.subnet_name
   resource_group_name  = module.resource_group.name
   virtual_network_name = azurerm_virtual_network.lights_on_heights_vnet.name
   address_prefixes     = var.subnet_address_prefixes
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+
 }
 
 # Association between Subnet and Network Security Group
